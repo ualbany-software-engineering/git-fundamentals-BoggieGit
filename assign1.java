735995cf3a0c4b7ea012a19cb3e3c71bd3c9d9d8
AJ <adelsonjules40@gmail.com>
2022-09-03 06:03:57 -0400

    Message

---
+++ assign1.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int x = 30;
+        int y = 3;
+        int a = 25;
+        final int b = x / y * a;
+        System.out.println("Hello, World!\n The full anwser is "+b); 
+    }
+}
